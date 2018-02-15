cf44e5338e0d2f35f044a2930d6e3c4f098574a8
LauraJaime8 <laurajaimev8@gmail.com>
2018-02-15 20:51:19 +0100

    Add components

---
+++ hello-world/src/components/global/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <h1>Footer - 2018</h1>
+    </div>
+    );
+  }
+}
+
+export default Footer;
